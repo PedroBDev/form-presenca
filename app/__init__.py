4772840fb0d75f9547b7f8781872a72538be5466
Pedro Barbosa <135766848+PedroBDev@users.noreply.github.com>
2026-01-30 22:37:15 -0300

    correção das variáveis de ambiente

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 import os
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = os.urandom(24)
-app.config['ADMIN_PASSWORD'] = "admin1234"
+app.config['SECRET_KEY'] = os.getenv("FLASK_SECRET_KEY")
+app.config['ADMIN_PASSWORD'] = os.getenv("ADMIN_PASSWORD")
 
 from app.routes import homepage, informacoes, admin
